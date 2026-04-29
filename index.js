506c76e4710410444456a6225dc38b6fbf2b72f2
Eduardo-Luiz-Campos <campos.santos.eduardo@escola.pr.gov.br>
2026-04-29 18:25:33 -0300

    Rotas do /pacientes funcionais com GET, POST, PUT e DELETE

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-"" 
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const pacientesRoutes = require('./routes/pacientes'); //exporta as rotas
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => { // Rota para enviar texto no diretório principal
+    res.send('Servidor da API de clinica está rodando com sucesso!');
+});
+
+app.use('/pacientes', pacientesRoutes); //Define que as rotas dos pacientes teram /pacientes antes
+
+app.listen(PORT, () => { // Para mostrar aonda o servidor está rodando no terminal e executar mais fácil
+    console.log(`Servidor rodando na porta http://localhost:${PORT}`);
+});
---
+++ routes/pacientes.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+const pathDados = path.join(__dirname, '../data/pacientes.json');
+
+// Função para ler dados
+const readData = () => {
+    const data = fs.readFileSync(pathDados);
+    return JSON.parse(data);
+};
+
+// Função para salvar dados
+const writeData = (data) => {
+    fs.writeFileSync(pathDados, JSON.stringify(data, null, 2));
+};
+
+// ________________________________________ ROTAS_________________________________________________________________________
+
+// GET - Listar todos os pacientes
+router.get('/', (req, res) => {
+    const pacientes = readData();
+    res.json(pacientes);
+});
+
+// Rota GET (ler) paciente por ID
+router.get('/:id', (req, res) => {  
+    const id = Number(req.params.id);
+    const pacientes = readData();
+    const paciente = pacientes.find(a => a.id === id);
+
+    if (!paciente) { //Verifica se existe o ID
+        return res.status(404).json({
+            erro: "paciente não encontrado",
+            mensagem: "Não existe nenhum paciente com esse Id"
+        });
+    };
+
+    res.json(paciente);
+});
+
+// POST - Criar novo paciente
+router.post('/', (req, res) => {
+    const { cpf, nome, telefone, idade } = req.body;
+
+    // Validação básica se existe ou  não
+    if (!nome || !cpf || !idade) {
+        return res.status(400).json({ erro: "CPF, Nome e idade são obrigatórios" });
+    }
+
+    const pacientes = readData();
+    const novoPaciente = {
+        id: pacientes.length > 0 ? pacientes[pacientes.length - 1].id + 1 : 1 ,
+        nome,
+        cpf,
+        telefone
+    };
+
+    pacientes.push(novoPaciente);
+    writeData(pacientes);
+
+    res.status(201).json(novoPaciente);
+});
+
+router.put('/:id', (req, res) => { // Rota PUT (modificar) algum paciente por ID
+    const id = Number(req.params.id);
+    const {  cpf, nome, telefone, idade } = req.body;
+    const pacientes = readData();
+    const index= pacientes.findIndex(a => a.id === id);
+
+    if (index === -1) { //Verifica se existe pelo ID
+        return res.status(404).json({
+            erro: "paciente não encontrado",
+            mensagem: "Não existe nenhum paciente com esse Id"
+        });
+    };
+
+    pacientes[index] = { id, cpf, nome, telefone, idade };
+
+    fs.writeFileSync('./data/pacientes.json', JSON.stringify(pacientes, null, 2));
+    res.json(pacientes[index]);
+});
+
+// DELETE - Remover
+router.delete('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    let pacientes = readData();
+    const existe = pacientes.some(a => a.id === id);
+
+    if (!existe) {
+        return res.status(404).json({ erro: "paciente não encontrado" });
+    }
+
+    pacientes = pacientes.filter(a => a.id !== id);
+    writeData(pacientes);
+    res.json({ mensagem: 'paciente removido!' });
+});
+
+module.exports = router;
